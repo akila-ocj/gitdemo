5f4e2d25c924f0fd63cef72bb3b64f7dbf417556
akila <akilaocj@gmail.com>
2024-05-01 16:06:35 +1000

    user 2

---
+++ src/Group1.java
@@ -0,0 +1,5 @@
+public class Group1 {
+    public static void main(String[] args) {
+        System.out.println("Hi I am from group 1");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,6 @@ public class Main {
         System.out.printf("Hello world");
         System.out.printf("I am user1");
 
+
     }
 }
